53116622e3e27f319304408d6b0839a5d42f0ab8
Thomas Bruce <44057980+the-Bruce@users.noreply.github.com>
2020-12-02 13:47:16 +0000

    Day 2 Solutions

--- src/Days/Day02.hs
+++ src/Days/Day02.hs
@@ -13,7 +13,6 @@ import qualified Util.Util as U
 
 import qualified Program.RunDay as R (runDay)
 import Data.Attoparsec.Text
-import Data.Void
 {- ORMOLU_ENABLE -}
 
 runDay :: Bool -> String -> IO ()
@@ -21,19 +20,42 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = ruleAndPassword `sepBy` endOfLine
+  where
+    ruleAndPassword = do
+      minv <- decimal
+      char '-'
+      maxv <- decimal
+      skipSpace
+      given <- letter
+      asciiCI ": "
+      password <- many1 letter
+      return Password {minv = minv, maxv = maxv, given = given, password = password}
 
 ------------ TYPES ------------
-type Input = Void
+data Password = Password {minv :: Int, maxv :: Int, given :: Char, password :: String} deriving (Show)
 
-type OutputA = Void
+type Input = [Password]
 
-type OutputB = Void
+type OutputA = Int
+
+type OutputB = Int
 
 ------------ PART A ------------
+passvalid1 :: Password -> Bool
+passvalid1 pass =
+  let len = length (filter ((\p x -> x == given p) pass) (password pass))
+   in len >= minv pass && len <= maxv pass
+
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA inp = length $ filter passvalid1 inp
 
 ------------ PART B ------------
+xor :: Bool -> Bool -> Bool
+xor a b = (a || b) && not (a && b)
+
+passvalid2 :: Password -> Bool
+passvalid2 pass = ((password pass !! (minv pass - 1)) == given pass) `xor` ((password pass !! (maxv pass - 1)) == given pass)
+
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB inp = length $ filter passvalid2 inp
