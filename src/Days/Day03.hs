38683b35a3dc202a7c7919e299cb8eeae39a48fd
Thomas Bruce <44057980+the-Bruce@users.noreply.github.com>
2020-12-03 12:14:34 +0000

    Tidy imports

--- src/Days/Day03.hs
+++ src/Days/Day03.hs
@@ -1,19 +1,11 @@
 module Days.Day03 (runDay) where
 
 {- ORMOLU_DISABLE -}
-import Data.List
 import Data.Map.Strict (Map, findWithDefault)
 import qualified Data.Map.Strict as Map
-import Data.Maybe
-import Data.Set (Set)
-import qualified Data.Set as Set
-import Data.Vector (Vector)
-import qualified Data.Vector as Vec
-import qualified Util.Util as U
 
 import qualified Program.RunDay as R (runDay)
 import Data.Attoparsec.Text
-import Data.Void
 
 import Util.Parsers (coordinateParser)
 {- ORMOLU_ENABLE -}
