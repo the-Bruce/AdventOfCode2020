1410145de2ee3e73ce6372130bb029605a873aec
Thomas Bruce <44057980+the-Bruce@users.noreply.github.com>
2020-12-05 13:50:12 +0000

    Day 5 solutions (first time using my own parser!)

--- src/Days/Day05.hs
+++ src/Days/Day05.hs
@@ -2,6 +2,7 @@ module Days.Day05 (runDay) where
 
 {- ORMOLU_DISABLE -}
 import Data.List
+import qualified Data.List as List
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
 import Data.Maybe
@@ -14,6 +15,7 @@ import qualified Util.Util as U
 import qualified Program.RunDay as R (runDay)
 import Data.Attoparsec.Text
 import Data.Void
+import Control.Applicative.Combinators ((<|>))
 {- ORMOLU_ENABLE -}
 
 runDay :: Bool -> String -> IO ()
@@ -21,19 +23,48 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = boarding `sepBy` endOfLine
+  where
+    boarding = do
+      row <- many1 (char 'F' <|> char 'B')
+      col <- many1 (char 'L' <|> char 'R')
+      return (row, col)
 
 ------------ TYPES ------------
-type Input = Void
+type Input = [(String, String)]
 
-type OutputA = Void
+type OutputA = Int
 
-type OutputB = Void
+type OutputB = Int
+
+type Range = (Int, Int)
 
 ------------ PART A ------------
+
+splitLow :: Range -> Range
+splitLow (a, b) = (a, a + ((b - a) `div` 2))
+
+splitHigh :: Range -> Range
+splitHigh (a, b) = (a + ((b - a + 1) `div` 2), b)
+
+splitIf :: Char -> (Range -> Range)
+splitIf 'B' = splitHigh
+splitIf 'F' = splitLow
+splitIf 'R' = splitHigh
+splitIf 'L' = splitLow
+
+pos :: (String, String) -> (Int, Int)
+pos (row, col) = (fst $ foldl (flip splitIf) (0, 127) row, fst $ foldl (flip splitIf) (0,7) col)
+
+pairID :: (Int, Int) -> Int
+pairID (a,b) = a*8+b
+
+seats :: [(String, String)] -> [Int]
+seats = map (pairID . pos)
+
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA inp = maximum $ seats inp
 
 ------------ PART B ------------
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB inp = head [x+1| x <- (seats inp), y <- (seats inp), y-x==2, x+1 `notElem` (seats inp)]
