ee4ee06f6f6b13482ecd194261f5fdba946dc411
Thomas Bruce <44057980+the-Bruce@users.noreply.github.com>
2020-12-10 12:50:49 +0000

    Day 8 (been busy past 2 days...)

--- src/Days/Day08.hs
+++ src/Days/Day08.hs
@@ -14,6 +14,8 @@ import qualified Util.Util as U
 import qualified Program.RunDay as R (runDay)
 import Data.Attoparsec.Text
 import Data.Void
+import Control.Applicative ((<|>))
+import Data.Functor (($>))
 {- ORMOLU_ENABLE -}
 
 runDay :: Bool -> String -> IO ()
@@ -21,19 +23,67 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = inst `sepBy` endOfLine
+  where
+    inst = do
+      op <- choice [string "acc" $> Acc, string "jmp" $> Jmp, string "nop" $> Nop]
+      string " "
+      sgn <- char '-' $> (\x -> (- x)) <|> char '+' $> id
+      arg <- decimal
+      return (op, sgn arg)
 
 ------------ TYPES ------------
-type Input = Void
+data Op = Acc | Jmp | Nop deriving (Show)
 
-type OutputA = Void
+type Program = [(Op, Int)]
 
-type OutputB = Void
+type Input = Program
+
+type OutputA = Int
+
+type OutputB = Int
+
+-- pc, acc
+type State = (Int, Int)
 
 ------------ PART A ------------
+updateState :: (Op, Int) -> State -> State
+updateState (Nop, _) state = (fst state + 1, snd state)
+updateState (Acc, arg) state = (fst state + 1, snd state + arg)
+updateState (Jmp, arg) state = (fst state + arg, snd state)
+
+runval :: Program -> State -> [Int] -> Int
+runval prog state exclude =
+  let newState = updateState (prog !! fst state) state
+   in if fst newState `elem` exclude
+        then snd newState
+        else runval prog newState (fst newState : exclude)
+
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA inp = runval inp (0, 0) []
 
 ------------ PART B ------------
+flipInst :: (Op, Int) -> (Op, Int)
+flipInst (Nop, a) = (Jmp, a)
+flipInst (Jmp, a) = (Nop, a)
+flipInst (Acc, a) = (Acc, a)
+
+flipProg' :: ([(Op, Int)], [(Op, Int)]) -> Program
+flipProg' (a, []) = a
+flipProg' (a, b) = a ++ [flipInst (head b)] ++ tail b
+
+flipProg :: Int -> Program -> Program
+flipProg index prog = flipProg' (splitAt index prog)
+
+runval' :: Program -> State -> [Int] -> Maybe Int
+runval' prog state exclude =
+  let newState = updateState (prog !! fst state) state
+   in if fst newState >= length prog
+        then Just (snd newState)
+        else
+          if fst newState `elem` exclude
+            then Nothing
+            else runval' prog newState (fst newState : exclude)
+
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB prog = head $ catMaybes [runval' (flipProg x prog) (0, 0) [] | x <- [-1 .. length prog - 1]]
